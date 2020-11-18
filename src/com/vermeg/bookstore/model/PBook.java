35b0ed066a034be2a076970f7ff6da38722c39f9
feriel-jridi-ESPRIT <feriel.jridi@esprit.tn>
2020-11-19 00:31:26 +0100

    my first commit

---
+++ src/com/vermeg/bookstore/model/PBook.java
@@ -0,0 +1,26 @@
+package com.vermeg.bookstore.model;
+
+public class PBook  extends Book{
+    private int nbrPage;
+
+    public PBook(){}
+
+    public PBook(int id, int quantity, String isbn, String title, String description, String photo, double price, int nbrPage) {
+        super(id, quantity, isbn, title, description, photo, price);
+        this.nbrPage = nbrPage;
+    }
+
+    public int getNbrPage() {
+        return nbrPage;
+    }
+
+    public void setNbrPage(int nbrPage) {
+        this.nbrPage = nbrPage;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "nbr pages" + this.nbrPage;
+    }
+
+}
