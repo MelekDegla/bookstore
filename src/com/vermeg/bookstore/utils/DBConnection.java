5c8401e52d272c14f925359a5e59f76a1ce75f61
melek <melek.degla@esprit.tn>
2020-11-18 18:48:22 +0100

    initialized project with Singleton connection to mysql

---
+++ src/com/vermeg/bookstore/utils/DBConnection.java
@@ -0,0 +1,35 @@
+package com.vermeg.bookstore.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection instance;
+
+    private final String URL = "jdbc:mysql://localhost:3306/bookstore";
+    private final String USERNAME= "root";
+    private final String PASSWORD= "";
+    private Connection connection;
+    private DBConnection() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("success");
+        }catch (SQLException | ClassNotFoundException exception){
+            exception.printStackTrace();
+        }
+
+    }
+
+    public static synchronized DBConnection getInstance(){
+        if(instance== null){
+            instance  = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
