890d253c13656fdc63a1b9ae8a69a29c3cc89d3f
BenAisia.Sami@esprit.tn <BenAissia.Sami@esprit.tn>
2020-11-18 23:19:38 +0100

    Eventfeature commited

--- src/com/vermeg/bookstore/Main.java
+++ src/com/vermeg/bookstore/Main.java
@@ -1,11 +1,16 @@
 package com.vermeg.bookstore;
 
+import com.vermeg.bookstore.model.Events;
+import com.vermeg.bookstore.service.EventsService;
 import com.vermeg.bookstore.utils.DBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.sql.Date;
+import java.sql.SQLException;
 
 public class Main  {
 
@@ -14,6 +19,8 @@ public class Main  {
 
     public static void main(String[] args) {
         DBConnection.getInstance().getConnection();
-//        launch(args);
-    }
+
+
+}
+
 }
